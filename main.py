a40f41fb584741fa86bdbbf856c310d596fe6ff1
Spencer Burris <whitephoenix0@users.noreply.github.com>
2016-06-08 15:42:47 -0700

    Remove rtfm,  fix formatting, add kappa

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ command_char = "!"
 
 # username = text.split[2]  "if username in list_of_users" should do it.
 
-f_words = ['Fucking', 'Fine', 'Fabulous', 'Frickin\'', 'Freaking', 'Fantastic', 'F\'n', 'Fugging', 'Fscking']
-
 volvo_jokes = ['What do you call a Volvo with dual exhaust? A wheel barrow!',
                'Why do Volvos have rear-window heaters? So your hands don\'t get cold when you have to push it!',
-               'What\'s the difference between a Volvo and the principal\'s office? It\'s less embarrassing if your friend see you leaving the principal\'s office.',
+               'What\'s the difference between a Volvo and the principal\'s office? It\'s less embarrassing if your friends see you leaving the principal\'s office.',
                'Why do they put sidewalks besides most streets and highways? So Volvo owners have a safe place to walk home.',
                'What\'s the difference between a Volvo and a Porcupine? When it comes to a Volvo, the prick is on the inside.',
                'How do you make a Volvo go faster downhill? Turn off the engine.',
@@ -37,13 +35,27 @@ crashes = ['segfaults and crashes',
            'gets a fatal null pointer exception',
            'dies at the hands of a memory leak',
            'breaks for no reason',
-           'crashes from crappy programming']
+           'crashes from crappy programming',
+	   'crashes after something goes wrong in factoryfactoryfactories.java',
+	   'gets an off-by-one error',
+	   'ponders whether his creator\'s choice to let him run pkill was a good idea'
+]
 
 linux_jokes = ['Linux is user-friendly; it\'s just picky about who its friends are.',
                'There are two major products of Berkeley. LSD and UNIX. Probably not a coincidence.',
                'Here\'s a nickel, kid. Get yourself a better computer.',
                'stop@hammertime~$ touch /this touch: cannot touch "/this": Permission denied.',
-               'rm -rf /bin/laden']
+               'rm -rf /bin/laden',
+	       'chown -R us ./allyourbase',
+	       'touch /woman touch: cannot touch \'woman\': Permission denied',
+	       '/home is where the /heart is.',
+	       'I want to dress up as a UDP packet for Halloween, but I don\'t know if anyone will get it.',
+	       'There is a Linux-themed resturaunt, if you ask for a Big Mac, they\'ll reply "FF:FF:FF:FF:FF:FF',
+	       'make love; Make: Don\'t know how to make love. Stop.',
+	       'man: why did you get a divorce?; man:: Too many arguments.'
+		]
+
+distro = ['Arch', 'Sabayon', 'Gentoo', 'Ubuntu', 'Fedora', 'Debian', 'Chakra', 'Void', 'Alpine', 'NixOS', 'CentOS']
 
 irc = IRC()
 
@@ -60,10 +72,6 @@ def if_command_action(trigger, response):
         irc.send_action(channel, response.encode())
 
 
-def rtfm():
-    return "Read The " + f_words[random.randint(0, 8)] + " Manual, you noob: https://wiki.archlinux.org/"
-
-
 def volvojoke():
     return volvo_jokes[random.randint(0, 8)]
 
@@ -77,17 +85,21 @@ def imply():
 
 
 def crash():
-    return crashes[random.randint(0, 8)]
+    return crashes[random.randint(0, 11)]
 
 
 def linuxjoke():
-    return linux_jokes[random.randint(0, 4)]
+    return linux_jokes[random.randint(0, 11)]
+
+def distro_list():
+    return distro[random.randint(0, 10)]
 
 while True:
     text = irc.get_text(channel)
     print(text)
 
-    if_command(command_char + "rtfm_test", rtfm())
+    if_command(".kappa", "ᕙ༼ಠ͜ʖಠ༽")
+    if_command("PhoenixBot3000, WAT DISTOR", distro_list())
     if_command(command_char + "volvojoke", volvojoke())
     if_command(command_char + "conspiracy", conspiracy())
     if_command("what os should I uninstall?", "WINDOWSWINDOWSWINDOWSWINDOWS WINDOOOOOOOOWS")
@@ -96,4 +108,4 @@ while True:
     if_command(command_char + "source", "https://github.com/whitephoenix0/phoenixbot")
     if_command(command_char + "cowbell", "https://www.reddit.com/r/needsmorecowbell/")
     if_command(command_char + "linuxjoke", linuxjoke())
-    if_command(command_char + "help", "rtfm_test, volvojoke, conspiracy, crash, implying, source, cowbell, linuxjoke, help")
+    if_command(command_char + "help", "volvojoke, conspiracy, crash, implying, source, cowbell, linuxjoke, help")
